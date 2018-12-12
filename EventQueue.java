fe67f2401da7ce8c371dd153e6df317f710e6d94
Tobias B. Rasch <tobiaz91@gmail.com>
2018-12-12 09:51:57 +0100

    Basic stuff set up

--- EventQueue.java
+++ EventQueue.java
@@ -1,3 +1,23 @@
 Public class EventQueue{
-    
+
+
+	public
+
+	// adds a new event to the queue
+	public void add(Event e){
+
+	}
+	//returns the next event in the queue
+	public Event next(){
+
+	}
+
+	//Checks if the queue has more events
+	public boolean hasNext(){
+
+	}
+
+
+
+
 }
---
+++ Population.java
@@ -0,0 +1,38 @@
+public class Population{
+
+
+
+
+
+	// adds i to this population
+	public  void add(Individual i){
+	}
+
+	// returns true if this population contains i;
+	public boolean contains(Individual i){
+
+	}
+	//removes i from this population, if it is there;
+	public void remove(Individual i){
+
+	}
+
+	//returns the number of individuals currently in this population;
+	public int size(){
+
+	}
+	// models an epidemic in this population;
+	public void epidemic(){
+
+	}
+
+	//returns the fitness of individual i in the population;
+	public double fitness(Individual i){
+
+	}
+
+	//returns a copy of the best path ever present in an Individual in this population
+	public City[] bestPath(){
+
+	}
+}
