710a2c0eaa415e5d3e0ce2f6eb9317f3d0edb2eb
Tobias B. Rasch <tobiaz91@gmail.com>
2018-12-13 13:05:55 +0100

    bufix, med oggi
    stavefejl og små ting fixed...

--- EventQueue.java
+++ EventQueue.java
@@ -1,43 +1,43 @@
 import java.util.ArrayList;
 public class EventQueue{
 	//Attributes
-	private ArrayList<Event> EvenetQueue;
+	private ArrayList<Event> eventQueue;
 
 
 	//Constructor
 	public EventQueue(){
-		EvenetQueue = new ArrayList<Event>();
+		eventQueue = new ArrayList<Event>();
 	}
 
 	// adds a new event to the queue
 	public void add(Event e){
-		if(EvenetQueue.size() == 0){
-			EvenetQueue.add(e);
+		if(eventQueue.size() == 0){
+			eventQueue.add(e);
 		}
-		if(EventQueue.get(0).time() < e.time()){
-			EvenetQueue.add(0,e);
+		else if(eventQueue.get(0).time() > e.time()){
+			eventQueue.add(0,e);
 		}
 
 		else{
-			for (int i = 1; i < EventQueue.size(); i++){
-				 if (EvenetQueue.get(i-1).time() <= e.time() && EvenetQueue.get(i+1).time() >= e.time()){
-					 EvenetQueue.add(i, e);
-				 }
+			int i = 1;
+			boolean isAdded = false;
+			while (i < eventQueue.size() && isAdded == false){
+				if (eventQueue.get(i-1).time() <= e.time() && eventQueue.get(i+1).time() >= e.time()){
+				 eventQueue.add(i, e);
+				 isAdded = true;
+				}
+				i ++;
 			}
 		}
 	}
 	//returns the next event in the queue and removes it
 	public Event next(){
-		Event nextEvent = EvenetQueue.get(0);
-		EvenetQueue.remove(0);
-		return nextEvent;
+		return eventQueue.remove(0);
 	}
 
 	//Checks if the queue has more events
 	public boolean hasNext(){
-		if(EventQueue.size() != 0){
-			retrun true;
-		}
+		return eventQueue.isEmpty();
 	}
 
 
